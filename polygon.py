3e1b6a623bb03011bbf1dfeaa324c8bc00f24d5c
Bryan Eriksson <bryaneriksson@uvic.ca>
2024-01-31 19:24:55 -0800

    Implemented properties

--- polygon.py
+++ polygon.py
@@ -1,9 +1,14 @@
 # polygon.py
+import math
+
 
 class Polygon():
-    def __init__(self, edges, cradius):
-        self._edges = edges
-        self._cradius = cradius
+    def __init__(self, edges: int, cradius: float):
+        self._edges = None
+        self._cradius = None
+
+        self.edges = edges
+        self.cradius = cradius
 
     @property
     def edges(self):
@@ -11,6 +16,8 @@ class Polygon():
     
     @edges.setter
     def edges(self, edges):
+        if edges < 3:
+            raise ValueError('Must be at least three edges/vertices.')
         self._edges = edges
     
     @property
@@ -19,9 +26,24 @@ class Polygon():
     
     @cradius.setter
     def cradius(self, cradius):
+        if cradius <= 0:
+            raise ValueError('Circumradius must be greater than zero.')
         self._cradius = cradius
 
-    
+    def iangle(self):
+        return (self.edges - 2) * (180 / self.edges)
+
+    def edge_length(self):
+        return 2 * self.cradius * math.sin(math.pi / self.edges)
+
+    def apothem(self):
+        return self.cradius * math.cos(math.pi / self.edges)
+
+    def area(self):
+        return (1 / 2) * self.edges * self.edge_length() * self.apothem()
+
+    def perimeter(self):
+        return self.edges * self.edge_length()
 
     def __repr__(self) -> str:
         pass
