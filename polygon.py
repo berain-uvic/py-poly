dbeec69fbaae87d62fe6184e95689ad50014c2a3
Bryan Eriksson <bryaneriksson@uvic.ca>
2024-01-31 20:19:18 -0800

    Class finished; some tests created

--- polygon.py
+++ polygon.py
@@ -3,54 +3,54 @@ import math
 
 
 class Polygon():
-    def __init__(self, edges: int, cradius: float):
-        self._edges = None
-        self._cradius = None
-
-        self.edges = edges
-        self.cradius = cradius
+    def __init__(self, n, R):
+        self._n = n
+        self._R = R
 
     @property
-    def edges(self):
-        return self._edges
-    
-    @edges.setter
-    def edges(self, edges):
-        if edges < 3:
-            raise ValueError('Must be at least three edges/vertices.')
-        self._edges = edges
+    def count_edges(self):
+        return self._n
     
     @property
-    def cradius(self):
-        return self._cradius
+    def count_vertices(self):
+        return self._n
     
-    @cradius.setter
-    def cradius(self, cradius):
-        if cradius <= 0:
-            raise ValueError('Circumradius must be greater than zero.')
-        self._cradius = cradius
+    @property
+    def circumradius(self):
+        return self._R
 
-    def iangle(self):
-        return (self.edges - 2) * (180 / self.edges)
+    @property
+    def interior_angle(self):
+        return (self._n - 2) * 180 / self._n
 
-    def edge_length(self):
-        return 2 * self.cradius * math.sin(math.pi / self.edges)
+    @property
+    def side_length(self):
+        return 2 * self._R * math.sin(math.pi / self._n)
 
+    @property
     def apothem(self):
-        return self.cradius * math.cos(math.pi / self.edges)
+        return self._R * math.cos(math.pi / self._n)
 
+    @property
     def area(self):
-        return (1 / 2) * self.edges * self.edge_length() * self.apothem()
+        return 1 / 2 * self._n * self.side_length() * self.apothem()
 
+    @property
     def perimeter(self):
-        return self.edges * self.edge_length()
+        return self._n * self.side_length()
 
     def __repr__(self) -> str:
-        pass
+        return f'Polygon(n={self._n}, R={self._R})'
 
-    def __eq__(self, __value: object) -> bool:
-        pass
+    def __eq__(self, other) -> bool:
+        if isinstance(other, Polygon):
+            return (self._n, self._R) == (other._n, other._R)
+        else:
+            return False
 
-    def __gt__():
-        pass
+    def __gt__(self, other) -> bool:
+        if isinstance(other, Polygon):
+            return self._n > other._n
+        else:
+            return False
 
